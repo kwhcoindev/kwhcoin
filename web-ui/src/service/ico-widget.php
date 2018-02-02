9a4a4222e1cb813399b30636700eab740a5cb2ad
Raj <rajgupta18@gmail.com>
2018-02-02 17:51:49 -0500

    Added ico widget

---
+++ web-ui/src/service/ico-widget.php
@@ -0,0 +1,68 @@
+<?php
+
+session_start();
+
+require("util.php");
+require("dbproperties.php");
+
+class TblIcoMaster {
+	public $cmwTicker;
+	public $coinAddress;
+	public $coinConstant;
+	public $coinCurrent;
+	public $coinName;
+	public $coinPlatformTicker;
+	public $coinTicker;
+	public $coinTotal;
+	public $date;
+	public $id;
+	public $insertDate;
+	public $updateby;
+}
+
+$status = 200;
+$list = array();
+$msg = "";
+
+header('Content-Type: application/json');
+cors();
+
+//Form post validation
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	echo json_encode(array( "status"=>500, "message"=>"Invalid request." ));
+    return;
+}
+
+$pdo = null;
+
+try{
+
+    $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+	
+	$pdo->beginTransaction();
+
+	$keys = array_keys($data); 
+	$fields = '`'.implode('`, `',$keys).'`';
+
+	#here is my way 
+	$placeholder = substr(str_repeat('?,',count($keys)),0,-1); 
+
+	$sql = "SELECT cmw_ticker as cmwTicker, coin_address as coinAddress, coin_constant as coinConstant, coin_current as coinCurrent, coin_name as coinName, coin_platform_ticker as coinPlatformTicker, coin_ticker as coinTicker, coin_total as coinTotal, date, id, insert_date as insertDate, updateby  from `tbl_ico_master`";
+	
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute();
+	$list = $stmt->fetchAll(PDO::FETCH_CLASS, "TblIcoMaster");
+
+	$stmt->closeCursor();
+	unset($stmt);
+	
+} catch (Exception $e) {
+	$msg = $e->getMessage();
+	$status = 500;
+	if(isset($pdo)) $pdo->rollback();
+}
+unset($pdo); 
+
+echo json_encode(array("status"=> $status, "message"=> $msg, "list"=> $list));
+
+?>
