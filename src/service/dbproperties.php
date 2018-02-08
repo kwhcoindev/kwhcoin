0366dc9857fee101a9d1f86a9771d031d68085e9
Raj <rajgupta18@gmail.com>
2018-02-07 22:50:12 -0500

    Rebasing with latest code

--- src/service/dbproperties.php
+++ src/service/dbproperties.php
@@ -1,13 +1,4 @@
 <?php 
 
-/*$hostname = "kwhdb.cholnwoifb8x.us-east-1.rds.amazonaws.com";
-$dbname = "kwh";
-$username = "kwhadmin";
-$pw = "W1nt3r2018!";
-*/
-$hostname = "cmwdevdb.cholnwoifb8x.us-east-1.rds.amazonaws.com";
-$dbname = "cmw";
-$username = "cmwdba";
-$pw = "cmwdba123!";
 
 ?>
