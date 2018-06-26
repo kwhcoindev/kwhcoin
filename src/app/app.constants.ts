ed8616c1a649aa2c3bc5a79bc39a3557e4c01def
Raj <rajgupta18@gmail.com>
2018-06-26 15:11:11 -0400

    Google map integration

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,11 +1,11 @@
 export class AppConstants {
-	public static API_URL: string = document.location.hostname.indexOf('kwhcoin.com') != -1? 'https://api.kwhcoin.com/' : 'http://dev.coinmarketwatch.com/kwh/';
+	public static API_URL: string = document.location.hostname.indexOf('APP_DOMAIN') != -1? 'APP_API_URL_PROD' : 'APP_API_URL_DEV';
 
-	public static API2_URL: string = document.location.hostname.indexOf('kwhcoin.com') != -1? 'https://api2.kwhcoin.com/' : 'http://35.170.160.153:8080/ico/';
+	public static API2_URL: string = document.location.hostname.indexOf('APP_DOMAIN') != -1? 'APP_API2_URL_PROD' : 'APP_API2_URL_DEV';
 
-	public static GoogleApiKey: string = "AIzaSyDGoLvy6GqPjkjDSx-XWsvp2gwy6gKRAcs";
+	public static GoogleApiKey: string = "KWH_GOOGLE_API_KEY";
 
-	public static Authorization: string = 'bcb66df33fbeec02931c0f99d84a3502b1146f3a';
+	public static Authorization: string = "KWH_APP_API_KEY";
 
 	public static Token: any = (()=>{
 		let token = null;
